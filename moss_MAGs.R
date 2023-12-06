d5eadf7cd4dbfd5fc261b56d18cabef6b4ed29fb
Jonathan Rondeau-Leclaire <97983060+jorondo1@users.noreply.github.com>
2023-12-06 11:19:10 -0500

    MAG_summary

---
+++ moss_MAGs.R
@@ -0,0 +1,15 @@
+library(pacman)
+p_load(phyloseq, tidyverse, magrittr, kable, kableExtra)
+source("myFunctions.R")
+
+MAG_summary <- inner_join(
+  # Final set of novel taxa found in at least one sample:
+  psMossMAGs@tax_table %>% data.frame %>% 
+    rownames_to_column("MAG"),
+  # MAG metrics
+  read_delim("data/novel_quality_scores.txt",
+             col_names = c('MAG', 'Completeness', 'Contamination', 'QualityScore')) %>% 
+            mutate(MAG = str_remove(MAG, '.fa')),
+          by = "MAG") %>% selecet(-Species)
+
+MAG_summary %>% write_tsv("out/MAG_summary.txt")
