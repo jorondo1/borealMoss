311538180b355cb98fee117392b138a815db6841
Jonathan Rondeau-Leclaire <97983060+jorondo1@users.noreply.github.com>
2024-01-18 15:31:24 -0500

    host pairwise DA, taxonomy correction

--- moss_PS_creation.R
+++ moss_PS_creation.R
@@ -4,10 +4,17 @@ source("myFunctions.R")
 
 # Sarah's PS object (based on Kraken taxonomic assignment)
 sarah.ps<- readRDS("data/ps_comptype.RDS")
+
+###################
+#### Abundance #####
+###################
+
 abund_GTDB <- parse_SM("data/SM_abund/*gtdb_gather.csv")
 abund_MAGs <- parse_SM("data/SM_abund/*custom_gather.csv")
 
-# GTDB Taxonomy
+##################
+#### Taxonomy #####
+##################
 taxLvls <- c("Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species")
 
 taxonomy <- read_delim("data/gtdbtk_summary.tsv") %>% 
@@ -38,6 +45,48 @@ taxonomy <- read_delim("data/gtdbtk_summary.tsv") %>%
       Species
     )) %>% select(-unique_MAGs)
 
+# Some taxonomic levels have redundancies because higher levels use alternative names,
+# herego there can be a duplicate Order whose Class or Phylum is different. Some 
+# can be heterotypic synonyms, others outdated taxonomic names.
+
+# Find duplicates; this function will open the viewer with the names to be resolved.
+listDupl <- function(tax, level) {
+  level_sym <- rlang::sym(level)
+  
+  subset <- tax %>% dplyr::select(Domain:!!level_sym) %>% 
+    dplyr::group_by(!!level_sym) %>%
+    unique
+  
+  dupList <- subset %>% 
+    dplyr::summarise(n = n()) %>%
+    dplyr::filter(n > 1) %>%
+    dplyr::pull(!!level_sym)
+    
+  subset %>% arrange(!!level_sym) %>% 
+    filter(!!level_sym %in% dupList) %>% print(n = 1000)
+}
+listDupl(taxonomy, "Genus")
+
+# Correct the different level duplicates, where "New_name" = "Old_name"
+corrPhylum <- c("Proteobacteria" = "Pseudomonadota",
+               "Actinomycetota" = "Actinobacteriota",
+               "Cyanobacteriota" = "Cyanobacteria")
+corrClass <- c("Terriglobia" = "Acidobacteriae")
+corrOrder <- c("Terriglobales" = "Acidobacteriales",
+               "Enterobacterales" = "Enterobacterales_A")
+corrFamily <- c("Enterobacteriaceae" = "Enterobacteriaceae_A",
+                "SZAS-18" = "UBA10450",
+                "Burkholderiaceae" = "Burkholderiaceae_B")
+# correct, but check family too...
+taxonomy %<>% mutate(Phylum = recode(Phylum, !!!corrPhylum),
+                    Class = recode(Class, !!!corrClass),
+                    Order = recode(Order, !!!corrOrder),
+                    Family = recode(Family, !!!corrFamily))
+
+##################
+#### Metadata #####
+##################
+
 # Species was used to designate Host Species, risk of confusion with Microbiome species
 sampleData <- sarah.ps %>% sample_data %>% as("data.frame") %>% 
   mutate(Host = str_c( 
@@ -52,6 +101,7 @@ psMossGTDB <- makePhyloSeq(abund_GTDB, sampleData, taxonomy)
 psMossMAGs <- makePhyloSeq(abund_MAGs, sampleData, taxonomy) %>% 
   # TEMPORARY : REMOVE MAG identified as contaminated by GUNC
   prune_taxa(taxa_names(.) !="Green_AO.bin.6",.)
+
 
 
 write_rds(psMossGTDB,"data/psMossGTDB.RDS")
