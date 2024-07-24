5a3bc907ac087943f80d5db949405ae54f1f535e
Jonathan Rondeau-Leclaire <97983060+jorondo1@users.noreply.github.com>
2024-07-24 14:29:04 -0400

    compile ratio in report

--- scripts/MAG_cont.sh
+++ scripts/MAG_cont.sh
@@ -1,6 +1,3 @@
-# fetch and index reference genomes 
-module load bowtie2/2.5.1
-
 # Index reference genome
 for MAG in $(find MAG_analysis/novel_species/genomes -type f -name '*.fa'); do
 	MAG_NAME=$(basename ${MAG%.fa})
@@ -10,4 +7,18 @@ for MAG in $(find MAG_analysis/novel_species/genomes -type f -name '*.fa'); do
 done
 
 # Align sample reads
-mkdir -p MAG_analysis/contamination_check
+mkdir -p MAG_analysis/contamination_check/logs
+export NOVEL="$MOSS"/MAG_analysis/novel_species
+export MAG_LIST="$NOVEL"/novel_MAGs.txt 
+export SAMPLE_LIST="$MOSS"/cat_reads/cat_samples.tsv
+export NUM_MAGS=$(wc $MAG_LIST | awk '{print $1}')
+export REF_GENOMES="$ANCHOR"/fast/def-ilafores/host_genomes
+export POLCOM_IDX="$REF_GENOMES"/PolCom_index/PolCom_genome
+export SOURMASH="singularity exec -e -B $ANCHOR/home:$ANCHOR/home -B $ANCHOR/fast:$ANCHOR/fast $ANCHOR/home/def-ilafores/programs/ILL_pipelines/containers/sourmash.4.7.0.sif sourmash"
+
+# if [[ ! -f $POLCOM_SIG ]]; then
+# 	$SOURMASH sketch dna -p scaled=1000,k=31 --name-from-first \
+# 	$REF_GENOMES/_genome_fasta/GCA_950295325.1_cbPolComm4.1_genomic.fna -o $POLCOM_SIG
+# fi
+sbatch --array=1-"${NUM_MAGS}" -n 6 --mem=2G \
+	$PWD/scripts/MAG_cont_SLURM.sh $NOVEL/genomes $MAG_LIST $SAMPLE_LIST $POLCOM_IDX
